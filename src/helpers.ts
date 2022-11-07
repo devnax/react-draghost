3688bab9c390bfb927197c0695f618b2627b946e
Naxrul Ahmed <devnaxrul@gmail.com>
2022-11-07 12:37:35 +0600

    Fix zindex

--- src/helpers.ts
+++ src/helpers.ts
@@ -31,21 +31,21 @@ if (typeof window !== 'undefined') {
 		document.head.appendChild(style)
 		style.innerHTML = `
 		.gu-mirror {
-			position: fixed !important;
-			margin: 0 !important;
-			z-index: 9999 !important;
+			position: fixed!important;
+			margin: 0!important;
+			z-index: 99999999999999!important;
 			opacity: 0.8;
 			-ms-filter: "progid:DXImageTransform.Microsoft.Alpha(Opacity=80)";
 			filter: alpha(opacity=80);
 		}
 		.gu-hide {
-			display: none !important;
+			display: none!important;
 		}
 		.gu-unselectable {
-			-webkit-user-select: none !important;
-			-moz-user-select: none !important;
-			-ms-user-select: none !important;
-			user-select: none !important;
+			-webkit-user-select: none!important;
+			-moz-user-select: none!important;
+			-ms-user-select: none!important;
+			user-select: none!important;
 		}
 		.gu-transit {
 			pointer-events: none;
